5416d2b9e55ab7a6802384f98c2fc63218b21969
marb61a <marb61a@gmail.com>
2018-09-10 16:44:44 +0100

    Added posts and single post dummy strucutre

---
+++ pages/posts/_id/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="single-post-page">
+    <section class="post">
+      <h1>Post Title</h1>
+      <div class="post-details">
+        <div>Last Updated on XXX</div>
+        <div>Post Written By</div>
+      </div>
+      <p>
+        Post Content
+      </p>
+    </section>
+    <section class="post-feedback">
+      <p>
+        Let me know your thoughts on the post
+      </p>
+    </section>
+  </div>
+</template>
