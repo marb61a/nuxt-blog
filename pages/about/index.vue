823a7ae9d2056a7065fb57ad005639c9e5187ded
marb61a <marb61a@gmail.com>
2018-09-10 16:32:10 +0100

    updated files and folders and added index.vue initial file

---
+++ pages/about/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="about-page">
+    <h1>This is the about page</h1>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+      Phasellus sed auctor purus.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+      Phasellus sed auctor purus.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+      Phasellus sed auctor purus.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+      Phasellus sed auctor purus.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+      Phasellus sed auctor purus.
+    </p>
+  </div>
+</template>
+
+<style scoped>
+  .about-page {
+    text-align: center;
+    padding: 30px;
+  }
+</style>
