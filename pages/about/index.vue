b062eb72f09711b6ef68cd9deab7545b620a2b92
marb61a <marb61a@gmail.com>
2018-09-11 11:35:09 +0100

    updated about index vue

--- pages/about/index.vue
+++ pages/about/index.vue
@@ -28,5 +28,43 @@
   .about-page {
     text-align: center;
     padding: 30px;
+    box-sizing: border-box;
   }
+
+  .post {
+    width: 100%;
+  }
+
+  @media (min-width: 768px) {
+    .post {
+      width: 600px;
+      margin: auto;
+    }
+  }
+
+  .post-title {
+    margin: 0;
+  }
+
+  .post-details {
+    padding: 10px;
+    box-sizing: border-box;
+    border-bottom: 3px solid rgb(243, 238, 238);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+  }
+
+  @media (min-width: 768px) {
+    .post-details {
+      flex-direction: row;
+    }
+  }
+
+  .post-detail {
+    color: rgb(88, 88, 88);
+    margin: 0 10px;
+  }
+
 </style>
