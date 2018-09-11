ea7f345f5e09ce6fae62e41ade998c0b8575b6ca
marb61a <marb61a@gmail.com>
2018-09-11 16:39:48 +0100

    updated header

--- components/Navigation/TheHeader.vue
+++ components/Navigation/TheHeader.vue
@@ -47,4 +47,50 @@
     box-sizing: border-box;
     padding: 0 20px;
   }
+
+  .logo {
+    margin: 0 10px;
+    font-size: 1.3rem;
+  }
+
+  .logo a {
+    text-decoration: none;
+    color: white;
+  }
+
+  .spacer {
+    flex: 1;
+  }
+
+  .navigation-items {
+    display: none;
+  }
+
+  @media (min-width: 768px) {
+    .navigation-items {
+      display: block;
+    }
+  }
+
+  .nav-list {
+    list-style: none;
+    padding: 0;
+    margin: 0;
+    display: flex;
+  }
+
+  .nav-item {
+    margin: 0 10px;
+  }
+
+  .nav-item a {
+    text-decoration: none;
+    color: white;
+  }
+
+  .nav-item a:hover,
+  .nav-item a:active,
+  .nav-item a.nuxt-link-active {
+    color: red;
+  }
 </style>
