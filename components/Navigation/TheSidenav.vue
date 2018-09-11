b5e05b7d4a11d422f7c99f97ea9b1455d09e0741
marb61a <marb61a@gmail.com>
2018-09-11 16:48:23 +0100

    added initial sidenav component

---
+++ components/Navigation/TheSidenav.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="sidenav-container">
+    <div
+      v-if="show"
+      class="sidenav-backdrop"
+      @click="$emit('close')"
+    >
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "TheSidenav",
+    props: {
+      show: {
+        type: Boolean,
+        default: false
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .sidenav-container {
+    height: 100%;
+    width: 100%;
+  }
+
+  .sidenav-backdrop {
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.7);
+    z-index: 1000;
+    position: fixed;
+    top: 0;
+    left: 0;
+  }
+
+</style>
