a849e3147f700919e1d61fbe04127e76f7a63959
marb61a <marb61a@gmail.com>
2018-09-11 11:46:48 +0100

    Updated post id style and added initial PostList component

---
+++ components/Posts/PostList.vue
@@ -0,0 +1,13 @@
+<template>
+  <section class="post-list">
+
+  </section>
+</template>
+
+<script>
+  import PostPreview from '@/components/Posts/PostPreview'
+
+  export default {
+
+  }
+</script>
--- pages/posts/_id/index.vue
+++ pages/posts/_id/index.vue
@@ -12,8 +12,65 @@
     </section>
     <section class="post-feedback">
       <p>
-        Let me know your thoughts on the post
+        Let me know your thoughts on the post by sending a mail to
+        <a href="mailto:feedback@my-awesome-domain.com">
+          feedback@my-awesome-domain.com
+        </a>
       </p>
     </section>
   </div>
 </template>
+
+<style>
+  .single-post-page {
+    padding: 30px;
+    text-align: center;
+    box-sizing: border-box;
+  }
+
+  .post {
+    width: 100%;
+  }
+
+  @media (min-width: 768px) {
+    .post {
+      width: 600px;
+      margin: auto;
+    }
+  }
+
+  .post-title {
+    margin: 0;
+  }
+
+  .post-details {
+    padding: 10px;
+    box-sizing: border-box;
+    border-bottom: 3px solid rgb(243, 238, 238);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+  }
+
+  @media (min-width: 768px) {
+    .post-details {
+      flex-direction: row;
+    }
+  }
+
+  .post-detail {
+    color: rgb(88, 88, 88);
+    margin: 0 10px;
+  }
+
+  .post-feedback {
+    color: red;
+    text-decoration: none;
+  }
+
+  .post-feedback a:hover,
+  .post-feedback a:active {
+    color: salmon;
+  }
+</style>
