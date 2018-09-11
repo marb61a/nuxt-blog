72a78c0bf9829b07cb67aa31718e499ad912e424
marb61a <marb61a@gmail.com>
2018-09-11 12:17:59 +0100

    Added style and section to PostList

--- components/Posts/PostList.vue
+++ components/Posts/PostList.vue
@@ -1,6 +1,23 @@
 <template>
   <section class="post-list">
-
+    <PostPreview
+      id="1"
+      thumbnail="https://static.pexels.com/photos/270348/pexels-photo-270348.jpeg"
+      title="Hello there!"
+      previewText="This my first post!"
+    />
+    <PostPreview
+      id="2"
+      thumbnail="https://static.pexels.com/photos/270348/pexels-photo-270348.jpeg"
+      title="Hello there - the second time!"
+      previewText="This my second post!"
+    />
+    <PostPreview
+      id="3"
+      thumbnail="https://static.pexels.com/photos/270348/pexels-photo-270348.jpeg"
+      title="Hi!"
+      previewText="This my third post!"
+    />
   </section>
 </template>
 
@@ -8,6 +25,22 @@
   import PostPreview from '@/components/Posts/PostPreview'
 
   export default {
+    components: {
+      PostPreview
+    },
+    props: {
 
+    }
   }
 </script>
+
+<style scoped>
+  .post-list {
+    display: flex;
+    padding: 20px;
+    box-sizing: border-box;
+    flex-wrap: wrap;
+    align-items: center;
+    justify-content: center;
+  }
+</style>
--- components/Posts/PostPreview.vue
+++ components/Posts/PostPreview.vue
@@ -18,7 +18,22 @@
   export default {
     name: 'PostPreview',
     props: {
-
+      id: {
+        type: String,
+        required: true
+      },
+      title: {
+        type: String,
+        required: true
+      },
+      previewText: {
+        type: String,
+        required: true
+      },
+      thumbnail: {
+        type: String,
+        required: true
+      },
     }
   }
 </script>
@@ -41,5 +56,22 @@
       width: 400px;
       margin: 10px;
     }
+  }
+
+  .post-thumbnail {
+    width: 100%;
+    height: 200px;
+    background-position: center;
+    background-size: cover;
+  }
+
+  .post-content {
+    padding: 10px;
+    text-align: center;
+  }
+
+  a:hover .post-content,
+  a:active .post-content {
+    background-color: #ccc;
   }
 </style>
