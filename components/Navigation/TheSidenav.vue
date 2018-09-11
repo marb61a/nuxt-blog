8a5bba4d6278e4c8bed351dc14a84205eaa96163
marb61a <marb61a@gmail.com>
2018-09-11 16:56:28 +0100

    added  sidenav component styling

--- components/Navigation/TheSidenav.vue
+++ components/Navigation/TheSidenav.vue
@@ -37,4 +37,46 @@
     left: 0;
   }
 
+  .sidenav {
+    height: 100%;
+    width: 300px;
+    background-color: white;
+    z-index: 10000;
+    position: fixed;
+    top: 0;
+    left: 0;
+    box-sizing: border-box;
+    padding: 30px;
+  }
+
+  .slide-side-enter-active,
+  .slide-side-leave-active {
+    transition: all 0.3s ease-out;
+
+  }
+  .slide-side-enter,
+  .slide-side-leave-to {
+    transform: translateX(-100%);
+  }
+
+  .nav-list {
+    list-style: none;
+    padding: 0;
+    margin: 0;
+  }
+
+  .nav-item {
+    margin: 20px 0;
+  }
+
+  .nav-item a {
+    text-decoration: none;
+    color: black;
+    font-size: 1.5rem;
+  }
+
+  .nav-item a:hover,
+  .nav-item a:active {
+    color: red;
+  }
 </style>
