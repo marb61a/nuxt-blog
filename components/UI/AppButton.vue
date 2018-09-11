ac590b1d0f5ff13a6e13d0841b9c969bbbdfd813
marb61a <marb61a@gmail.com>
2018-09-11 17:18:32 +0100

    added Appbutton UI component

---
+++ components/UI/AppButton.vue
@@ -0,0 +1,40 @@
+<template>
+  <button
+    class="button"
+    :class="btnStyle"
+    v-bind="$attrs"
+    v-on="$listeners"
+  >
+    <slot />
+  </button>
+</template>
+
+<script>
+  export default {
+    name: "AppButton",
+    props: {
+      btnStyle: {
+        type: String,
+        default: ''
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .button {
+    font: inherit;
+    background-color: black;
+    color: white;
+    padding: 5px;
+    cursor: pointer;
+    border: 1px solid black;
+  }
+
+  .button:hover,
+  .button:active {
+    background-color: rgb(51, 51, 51);
+    border: 1px solid black;
+  }
+
+</style>
