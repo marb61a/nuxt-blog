57f3c56754987c719becebbacb9fdda6add85432
marb61a <marb61a@gmail.com>
2018-09-11 17:03:36 +0100

    added sidenavtoggle component

--- components/Navigation/TheSidenav.vue
+++ components/Navigation/TheSidenav.vue
@@ -6,6 +6,18 @@
       @click="$emit('close')"
     >
     </div>
+    <transition name="slide-side">
+      <div
+        v-if="show"
+        class="sidenav"
+      >
+        <ul class="nav-list" @click="$emit('close')">
+          <li class="nav-item"><nuxt-link to="/posts">Blog</nuxt-link></li>
+          <li class="nav-item"><nuxt-link to="/about">About</nuxt-link></li>
+          <li class="nav-item"><nuxt-link to="/admin">Admin</nuxt-link></li>
+        </ul>
+      </div>
+    </transition>
   </div>
 </template>
 
