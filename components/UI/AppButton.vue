183ee222c5c891a31e5cf9084223d51b8dc0d141
marb61a <marb61a@gmail.com>
2018-09-11 20:54:13 +0100

    added AppControlInput UI component

--- components/UI/AppButton.vue
+++ components/UI/AppButton.vue
@@ -37,4 +37,29 @@
     border: 1px solid black;
   }
 
+  .inverted {
+    background-color: white;
+    color: black;
+    border: 1px solid transparent;
+  }
+
+  .inverted:hover,
+  .inverted:active {
+    color: #ccc;
+    background-color: white;
+    border: 1px solid transparent;
+  }
+
+  .cancel {
+    background-color: red;
+    color: white;
+    border: 1px solid transparent;
+  }
+
+  .cancel:hover,
+  .cancel:active {
+    background-color: salmon;
+    border: 1px solid transparent;
+    color: white;
+  }
 </style>
---
+++ components/UI/AppControlInput.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="input-control">
+
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'AppInputControl',
+    props: {
+
+    }
+  }
+</script>
+
+<style scoped>
+  .input-control {
+    margin: 10px 0;
+  }
+</style>
