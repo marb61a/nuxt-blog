3a3b11e617972d7538443f5bf4d4b42f3c45eb38
marb61a <marb61a@gmail.com>
2018-09-12 16:43:09 +0100

    added adminpostform

---
+++ components/Admin/AdminPostForm.vue
@@ -0,0 +1,31 @@
+<template>
+  <form @submit.prevent="onSave">
+    <AppControlInput v-model="editedPost.author">
+      Author Name
+    </AppControlInput>
+    <AppControlInput v-model="editedPost.title">
+      Title
+    </AppControlInput>
+    <AppControlInput v-model="editedPost.thumbnailLink">
+      Thumbnail Link
+    </AppControlInput>
+  </form>
+</template>
+
+<script>
+  import AppControlInput from "@/components/UI/AppControlInput";
+  import AppButton from "@/components/UI/AppButton";
+
+  export default {
+    components: {
+      AppControlInput,
+      AppButton
+    },
+    props: {
+      post: {
+        type: Object,
+        required: false
+      }
+    }
+  }
+</script>
