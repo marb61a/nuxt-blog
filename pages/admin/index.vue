8ec17f538c6376e9598925aa043f883a6e5d7a3f
marb61a <marb61a@gmail.com>
2018-09-12 20:48:13 +0100

    added admin index

---
+++ pages/admin/index.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="admin-page">
+    <section class="new-post">
+
+    </section>
+  </div>
+</template>
+
+<script>
+  import PostList from '@/components/Posts/PostList';
+  import AppButton from '@/components/UI/AppButton';
+
+  export default {
+    layout: 'admin',
+    components: {
+      PostList,
+      AppButton
+    }
+  }
+</script>
+
+<style scoped>
+  .admin-page {
+    padding: 20px;
+  }
+
+  .new-post {
+    text-align: center;
+    border-bottom: 2px solid #ccc;
+    padding-bottom: 10px;
+  }
+
+  .existing-posts h1 {
+    text-align: center;
+  }
+</style>
