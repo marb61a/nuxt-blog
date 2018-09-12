e6dbc881225362344a394246c6468f7726869b0c
marb61a <marb61a@gmail.com>
2018-09-12 16:50:52 +0100

    added data and methods to adminpostform admin component

--- components/Admin/AdminPostForm.vue
+++ components/Admin/AdminPostForm.vue
@@ -9,6 +9,21 @@
     <AppControlInput v-model="editedPost.thumbnailLink">
       Thumbnail Link
     </AppControlInput>
+    <AppControlInput
+      control-type="textarea"
+      v-model="editedPost.content"
+    >
+      Content
+    </AppControlInput>
+    <AppButton>Save</AppButton>
+    <AppButton
+      type="button"
+      style="margin-left: 10px"
+      btn-style="cancel"
+      @click="onCancel"
+    >
+      Cancel
+    </AppButton>
   </form>
 </template>
 
@@ -25,6 +40,28 @@
       post: {
         type: Object,
         required: false
+      }
+    },
+    data() {
+      return {
+        editedPost: this.post ?
+        { ...this.post } :
+        {
+          author: "",
+          title: "",
+          thumbnailLink: "",
+          content: ""
+        }
+      }
+    },
+    methods: {
+      onSave(){
+        // Save the post
+        console.log(this.editedPost);
+      },
+      onCancel(){
+        // Navigate back
+        this.$router.push("/admin");
       }
     }
   }
