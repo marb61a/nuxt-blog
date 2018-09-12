8479e274330350f8db3e53502ccf56aaa950a3b4
marb61a <marb61a@gmail.com>
2018-09-12 20:56:11 +0100

    added auth  index

---
+++ pages/admin/auth/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="admin-auth-page">
+    <div class="auth-container">
+
+    </div>
+  </div>
+</template>
+
+<script>
+  import AppControlInput from '@/components/UI/AppControlInput';
+  import AppButton from '@/components/UI/AppButton';
+
+  export default {
+    name: 'AdminAuthPage',
+    layout: 'admin',
+    components: {
+      AppControlInput,
+      AppButton
+    },
+    data() {
+      return {
+        isLogin: true
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .admin-auth-page {
+    padding: 20px;
+  }
+
+</style>
--- pages/admin/index.vue
+++ pages/admin/index.vue
@@ -1,7 +1,13 @@
 <template>
   <div class="admin-page">
     <section class="new-post">
-
+      <AppButton @click="$router.push('/admin/new-post')">
+        Create Post
+      </AppButton>
+    </section>
+    <section class="existing-posts">
+      <h1>Existing Posts</h1>
+      <PostList isAdmin />
     </section>
   </div>
 </template>
