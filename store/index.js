7bad7c31e8840080f7ea6609a31bc2c1452ab670
marb61a <marb61a@gmail.com>
2018-09-13 12:17:17 +0100

    added initial store

---
+++ store/index.js
@@ -0,0 +1,31 @@
+import Vuex from 'vue';
+
+const createStore = () => {
+  return new Vuex.Store({
+    state: {
+      loadedPosts: []
+    },
+    mutations: {
+      setPosts(state, posts) {
+        state.loadedPosts = posts;
+      },
+      addPost(state, post) {
+        state.loadedPosts.push(post);
+      },
+      editPost(state, editedPost) {
+        const postIndex = state.loadedPosts.findIndex(
+          post => post.id === editedPost.id
+        );
+        state.loadedPosts[postIndex] = editedPost
+      }
+    },
+    actions: {
+
+    },
+    getters: {
+
+    }
+  });
+};
+
+export default createStore;
