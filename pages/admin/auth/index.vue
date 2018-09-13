a4979d2040a0a676c59b4887cfdc8ef0b913839e
marb61a <marb61a@gmail.com>
2018-09-13 11:29:11 +0100

    updated auth  index

--- pages/admin/auth/index.vue
+++ pages/admin/auth/index.vue
@@ -1,7 +1,21 @@
 <template>
   <div class="admin-auth-page">
     <div class="auth-container">
-
+      <form>
+        <AppControlInput type="email">E-Mail Address</AppControlInput>
+        <AppControlInput type="password">Password</AppControlInput>
+        <AppControlInput type="submit">
+          {{ isLogin ? 'Login' : 'Sign Up' }}
+        </AppControlInput>
+        <AppButton
+          type="button"
+          btn-style="inverted"
+          style="margin-left: 10px"
+          @click="isLogin = !isLogin"
+        >
+          Switch to {{ isLogin ? 'Signup' : 'Login' }}
+        </AppButton>
+      </form>
     </div>
   </div>
 </template>
@@ -30,4 +44,13 @@
     padding: 20px;
   }
 
+  .auth-container {
+    border: 1px solid #ccc;
+    border-radius: 5px;
+    box-shadow: 0 2px 2px #ccc;
+    width: 300px;
+    margin: auto;
+    padding: 10px;
+    box-sizing: border-box;
+  }
 </style>
